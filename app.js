17f88145d122bedbfd04c326e3eeae8faaa4b4a0
Mohamed Alhamry <alhamrimohammed4@email.com>
2025-02-04 09:02:03 +0300

    Exercise 9

--- app.js
+++ app.js
@@ -188,3 +188,39 @@ function convertTemperature(temp, scale) {
 }
 
 console.log('Exercise 8 Result:', convertTemperature(32, "K"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+function basicCalculator(num1, num2, operation) {
+    if(operation === 'add') {
+        return num1 + num2;
+    } else if(operation === 'subtract') {
+        return num1 - num2;
+    }  else if(operation === 'multiply') {
+        return num1 * num2;
+    }  else if(operation === 'divide') {
+        if (num2 === 0) {
+            return 'Invalid input: Cannot divide by zero.';
+        }
+        return num1 / num2;
+    } else {
+        return "Invalid input: Please provide a valid operation ('add', 'subtract', 'multiply', or 'divide).";
+    }
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
