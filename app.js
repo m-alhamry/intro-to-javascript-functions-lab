8b33afaa0f049fc334f71a404caa3059fd254fde
Mohamed Alhamry <alhamrimohammed4@email.com>
2025-02-04 08:33:39 +0300

    Exercise 5

--- app.js
+++ app.js
@@ -91,3 +91,32 @@ function generateEmail(name, domain) {
 }
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser(name, timeOfDay) {
+    const times = ['morning', 'afternoon', 'evening'];
+    name = name.trim();
+
+    if (typeof name !== 'string') {
+        return 'Invalid input: Please provide a valid name.';
+    }
+
+    if (!times.includes(timeOfDay)) {
+        return 'Invalid input: Time of day must be "morning", "afternoon", or "evening".';
+    }
+    
+    return `Good ${timeOfDay}, ${name}!`;
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
